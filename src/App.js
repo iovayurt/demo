07235c87e599b5a36c52611debbee02be592be9a
Tahir Ovayurt <iovayurt@gmail.com>
2022-03-05 22:54:15 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ import Logo from "./assets/money.png";
 
 import {Container, Row, Nav, Navbar, Card, NavDropdown} from 'react-bootstrap';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import getConfig from './config'
 import reactDom from 'react-dom';
 const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
 export default function App() {
   return (
-    <React.Fragment>
+    <Router>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home"><img src={Logo} /></Navbar.Brand>
+          <Navbar.Brand href="/"><img src={Logo} /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto"></Nav>
-              <Nav.Link>Transactions</Nav.Link>
+              <Nav.Link href="/transactions">Transactions</Nav.Link>
               <Nav.Link
                 onClick={window.walletConnection.isSignedIn() ? logout : login}
               >
@@ -34,7 +41,25 @@ export default function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    </React.Fragment>
+      <Container>
+        {window.walletConnection.isSignedIn() ? (
+        <Row>
+          <Switch>
+            <Route exact path='/'>MoneyMemo</Route>
+            <Route exact path='/transactions'>Transactions</Route>
+          </Switch>
+        </Row>
+        ):(
+        <Row>
+            <Card>
+              <Card.Body>
+                <Card.Title>Please Sign In</Card.Title>
+              </Card.Body>
+            </Card>
+          </Row>
+        )}
+      </Container>
+    </Router>
   );
 
 }
