a693dfd5e5fb309d6000584b836dda69ad62074b
Tahir Ovayurt <iovayurt@gmail.com>
2022-03-10 22:30:51 +0300

    change

--- src/App.js
+++ src/App.js
@@ -22,43 +22,7 @@ const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 export default function App() {
   return (
     <Router>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/"><img src={Logo} /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto"></Nav>
-              <Nav.Link href="/transactions">Transactions</Nav.Link>
-              <Nav.Link
-                onClick={window.walletConnection.isSignedIn() ? logout : login}
-              >
-                {window.walletConnection.isSignedIn()
-                  ? window.accountId
-                  : "Login"
-                }
-                </Nav.Link>
 
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Container>
-        {window.walletConnection.isSignedIn() ? (
-        <Row>
-          <Switch>
-            <Route exact path='/'>MoneyMemo</Route>
-            <Route exact path='/transactions'>Transactions</Route>
-          </Switch>
-        </Row>
-        ):(
-        <Row>
-            <Card>
-              <Card.Body>
-                <Card.Title>Please Sign In</Card.Title>
-              </Card.Body>
-            </Card>
-          </Row>
-        )}
-      </Container>
     </Router>
   );
 
