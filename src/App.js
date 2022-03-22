d1c8d15830782b2dc7101a6f34d5f14c02f89034
Tahir Ovayurt <iovayurt@gmail.com>
2022-03-22 22:48:17 +0300

    button

--- src/App.js
+++ src/App.js
@@ -58,14 +58,6 @@ export default function App() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
-        <div className="d-grid gap-2">
-          <Button variant="primary" size="lg">
-            Block level button
-          </Button>
-          <Button variant="secondary" size="lg">
-            Block level button
-          </Button>
-        </div>
     </Router>
   );
 
